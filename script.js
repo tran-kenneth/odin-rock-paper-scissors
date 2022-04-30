d9183c1fdfdaeba4005802558038dc3e04b5962a
Kenneth Tran <kennetht787@gmail.com>
2022-04-30 18:31:49 -0400

    Add pseudocode and requirements as block comment

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+  Rock Paper Scissors (RPS) Game Pseudocode
+
+  function computerPlay -> returns rock, paper or scissors 
+  function playRound -> plays a single round of RPS, take two params: playerSelection, computerPlay
+    returns string to declare winner "You Lose/Win. X beats Y"
+
+  function playerSelection -> takes player selection, make it case insensitive!
+
+  function game() -> calls playRound, to play 5 rounds, keep score.
+
+  Use prompt() to get user input. 
+  Use console.log() to display results
+
+*/
